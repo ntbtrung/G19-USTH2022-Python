d38cd87e132e050b7f00acddc836ac3ec8c3931f
t <jeremyraberdham@gmail.com>
2022-04-28 21:41:30 +0700

    Classes

---
+++ domains.py
@@ -0,0 +1,60 @@
+
+class Passenger:
+    def __init__(self, name, pid, f_code, seat):
+        self.name = name 
+        # Name; e.g.: Vu Quoc Thai
+        self.pid = pid 
+        # Personal ID; e.g.: 001202001264
+
+        self.f_code = f_code
+        # Flight code; e.g: VN457
+        self.seat = seat 
+        # Seat number; e.g.: F3, F5, F6
+
+class Plane:
+    def __init__(self, name, f_code):
+        self.name = name
+        # Name; e.g.: Boeing 757
+
+        self.f_code = f_code
+        ###
+        # Flight code; e.g: VN457
+        # To be sychronised with Passenger through Flight class
+        ###
+
+class Flight:
+    def __init__(self, f_code, 
+    n_passenger, list_passenger, 
+    t_depart, t_arrive, 
+    p_depart, p_arrive,
+    ):
+        self.f_code = f_code
+        ###
+        # Flight code; e.g: VN457
+        # To be sychronised with Passenger and Plane
+        ###
+
+        self.n_passenger = n_passenger
+        self.list_passenger = list_passenger
+        ###
+        # Variables to manage passenger
+        ###
+
+        self.t_depart = t_depart
+        self.t_arrive = t_arrive
+        ###
+        # Variables for depart and arrive time
+        # Use 24hr format
+        ###
+
+        self.p_depart = p_depart
+        self.p_arrive = p_arrive
+        ###
+        # Variables for depart and arrive location
+        # Use format: City, Country
+        ###
+
+
+
+
+
